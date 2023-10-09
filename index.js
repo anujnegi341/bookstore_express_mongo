2ed177a12af2b1ff73889f0778eb3be80be62cf1
Anuj Negi <negi.anuj341@gmail.com>
2023-10-10 00:30:44 +0530

    add api for update and delete books

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ app.post('/books', async (req, res) => {
     }
 })
 
-
 app.get('/books', async (req, res) => {
     try {
         const books = await Book.find({});
@@ -51,7 +50,6 @@ app.get('/books', async (req, res) => {
     }
 })
 
-
 app.get('/books/:id', async (req, res) => {
     try {
 
@@ -59,6 +57,47 @@ app.get('/books/:id', async (req, res) => {
         const book = await Book.findById(id);
 
         return res.status(200).json(book);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({message: error.message});
+    }
+})
+
+app.put('/books/:id', async (req, res) => {
+    try {
+
+        if(!req.body.title || !req.body.author || !req.body.price) {
+            return res.status(400)
+                .send({
+                    message : 'Send all required data that is: Title, Author and Price'
+                });
+        }
+
+        const { id } = req.params
+        const book = await Book.findByIdAndUpdate(id, req.body);
+
+        if(!book) {
+            return res.status(404).send('Id not found');
+        } else {
+            return res.status(200).send('Book updated successfully');
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({message: error.message});
+    }
+})
+
+app.delete('/books/:id', async (req, res) => {
+    try {
+
+        const { id } = req.params
+        const book = await Book.findByIdAndDelete(id);
+
+        if(!book) {
+            return res.status(404).send('Id not found')
+        } 
+
+        return res.status(200).send('Book deleted successfully');
     } catch (error) {
         console.log(error);
         return res.status(500).send({message: error.message});
