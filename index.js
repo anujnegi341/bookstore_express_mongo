3408704a6f21c215f432ee1dce08e7eacfb22eae
Anuj Negi <negi.anuj341@gmail.com>
2023-10-10 00:48:17 +0530

    add cors package

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ import express from "express";
 import mongoose from "mongoose";
 import { PORT, mongodbUrl } from "./config.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:8080',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}))
 
 app.use('/books', booksRoute);
 
