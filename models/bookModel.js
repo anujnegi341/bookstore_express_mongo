b28ffc1b0dc369af7dde0dc0535d400a67ed0a71
Anuj Negi <negi.anuj341@gmail.com>
2023-10-18 23:30:50 +0530

    udpate review schema

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,5 +1,28 @@
 import mongoose from "mongoose";
-import { reviewSchema } from "./reviewModel.js";
+
+export const reviewSchema = mongoose.Schema(
+    {   
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        reviewText: {
+            type: String,
+            required: true
+        },
+        rating: {
+          type: Number,
+          required: true,
+          min: 1,
+          max: 5
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+    }
+)
 
 const bookSchema = mongoose.Schema(
     {
