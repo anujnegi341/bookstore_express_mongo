0415a8c96b7d03801821f430d4e72f9c7b465434
Anuj Negi <negi.anuj341@gmail.com>
2023-11-08 01:33:01 +0530

    feat: add authentication through middleware

--- index.js
+++ index.js
@@ -8,9 +8,37 @@ import cors from 'cors';
 
 const app = express();
 
+const myLogger = (req, res, next) => {
+    console.log(Date.now());
+    next();
+}
+
+const authentication = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        //Authorization: 'Bearer TOKEN'
+        if (!token) {
+            res.status(400).json({ success: false, message: "Error! Token was not provided." });
+        }
+        //Decoding the token
+        const decodedToken = jwt.verify(token, jwtSecret);
+
+        req.user = {
+                userId: decodedToken.userId,
+                email: decodedToken.email
+            }
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(401).send({ message: error.message });
+    }
+}
+
 app.use(express.json());
 
 app.use(cors());
+
+app.use('/books', myLogger)
 
 // app.use(cors({
 //     origin: 'http://localhost:8080',
@@ -18,10 +46,8 @@ app.use(cors());
 //     allowedHeaders: ['Content-Type']
 // }))
 
-app.use('/books', booksRoute);
-
 app.get('/', (req, res) => {
-    console.log(req);
+    console.log('Body : >> ', req.body);
     return res.status(200).send("Plwease sign up first !!");
 })
 
@@ -86,6 +112,8 @@ app.post("/login", async (req, res) => {
     }
 });
 
+app.use(authentication);
+
 app.get('/accessResource', (req, res) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
@@ -108,6 +136,8 @@ app.get('/accessResource', (req, res) => {
         res.status(500).send({ message: error.message });
     }
 })
+
+app.use('/books', booksRoute);
 
 
 mongoose
